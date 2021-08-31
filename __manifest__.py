fea0e32892ebef7b0505fa283d9b1aff0063efa7
Moises Edson Yañez Villa <edson.yanez@sincpro.com.bo>
2021-08-30 20:32:51 -0400

    add xml whatsapp button

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Sincpro Whatsapp',
+    'description': '',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/view_move_form_whatsapp_button.xml',
+    ],
+}
