bfd2bc6b143b38fd5ae410a0d58b9a0c6b8d0ee9
jairo sandoval <jairo.sandoval.velasquez@gmail.com>
2021-09-24 11:20:52 -0700

    feature button save

--- models/settings.py
+++ models/settings.py
@@ -1,6 +1,27 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
-class WhatsappSettings(models.TransientModel):
+class WhatsappConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    note = fields.Char(string="Default Note")
+    whatsapp_crm= fields.Boolean("")
+    whatsapp_sales = fields.Boolean("")
+    whatsapp_inventory = fields.Boolean("")
+
+    def set_values(self):
+        res = super(WhatsappConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_crm', self.whatsapp_crm)
+        self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_sales', self.whatsapp_sales)
+        self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_inventory', self.whatsapp_inventory)
+        return res
+
+    def get_values(self):
+        res = super(WhatsappConfigSettings, self).get_values()
+        whatsapp_crm = self.env['ir.config_parameter'].get_param("whatsapp.whatsapp_crm")
+        whatsapp_sales = self.env['ir.config_parameter'].get_param("whatsapp.whatsapp_sales")
+        whatsapp_inventory = self.env['ir.config_parameter'].get_param("whatsapp.whatsapp_inventory")
+        res.update({
+            'whatsapp_crm': whatsapp_crm,
+            'whatsapp_sales': whatsapp_sales,
+            'whatsapp_inventory': whatsapp_inventory
+        })
+        return res
