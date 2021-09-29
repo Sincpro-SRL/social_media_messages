4e2d5a438453ed8c0430eecf9cdc6428a65e5d10
jairo sandoval <jairo.sandoval.velasquez@gmail.com>
2021-09-29 14:17:03 -0700

    token

--- models/settings.py
+++ models/settings.py
@@ -6,12 +6,14 @@ class WhatsappConfigSettings(models.TransientModel):
     whatsapp_crm= fields.Boolean("")
     whatsapp_sales = fields.Boolean("")
     whatsapp_inventory = fields.Boolean("")
+    whatsapp_token = fields.Char(string="token")
 
     def set_values(self):
         res = super(WhatsappConfigSettings, self).set_values()
         self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_crm', self.whatsapp_crm)
         self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_sales', self.whatsapp_sales)
         self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_inventory', self.whatsapp_inventory)
+        self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_token', self.whatsapp_token)
         return res
 
     def get_values(self):
@@ -19,9 +21,11 @@ class WhatsappConfigSettings(models.TransientModel):
         whatsapp_crm = self.env['ir.config_parameter'].get_param("whatsapp.whatsapp_crm")
         whatsapp_sales = self.env['ir.config_parameter'].get_param("whatsapp.whatsapp_sales")
         whatsapp_inventory = self.env['ir.config_parameter'].get_param("whatsapp.whatsapp_inventory")
+        whatsapp_token = self.env['ir.config_paramenter'].get_param("whatsapp.whatsapp_token")
         res.update({
             'whatsapp_crm': whatsapp_crm,
             'whatsapp_sales': whatsapp_sales,
-            'whatsapp_inventory': whatsapp_inventory
+            'whatsapp_inventory': whatsapp_inventory,
+            'whatsapp_token': whatsapp_token,
         })
         return res
