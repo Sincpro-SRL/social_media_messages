7eb2db3b7a03381f3315c89a1cf2d725f0904e4c
Jeffer <jeffer.barrera@sincpro.com.bo>
2021-10-12 16:41:06 -0500

    Create new contact from webhook messenger

--- models/contact_facebook_profile.py
+++ models/contact_facebook_profile.py
@@ -3,7 +3,23 @@
 from odoo import models, fields, api
 
 
-class AddInfoProfile(models.Model):
+class AddIdFacebook(models.Model):
     _inherit = 'res.partner'
 
-    ID_facebook = fields.Char('ID perfil Facebook')
+    id_facebook = fields.Char('ID perfil Facebook', readonly=True)
+    from_messenger = fields.Boolean('Obtenido desde messenger', readonly=True)
+
+
+class CreateContactMessenger(models.Model):
+    _name = 'create.contact'
+    _description = 'Creacion de contacto desde el Webhook de messenger'
+    _auto = False
+
+    @api.model
+    def create_partner_webhook_event(self, dic_data):
+        values = {
+            'name': f"{dic_data['first_name']} {dic_data['last_name']}",
+            'id_facebook': dic_data['id'],
+            'from_messenger': True
+        }
+        self.env['res.partner'].create(values)
