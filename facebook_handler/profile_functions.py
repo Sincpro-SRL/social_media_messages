d74b0e138a33b533d83edf055f65fe0db8d7e6a0
jairo sandoval <jairo.sandoval.velasquez@gmail.com>
2021-10-11 19:24:58 -0700

    .

--- facebook_handler/profile_functions.py
+++ facebook_handler/profile_functions.py
@@ -3,11 +3,8 @@ import requests
 def get_info_profile(user, token):
     user_id = user["sender"]["id"]
 
-    if not token:
-        return token
-    else:
-        url = f'https://graph.facebook.com/{user_id}?access_token={token}'
-        response = requests.get(url)
-        return response.json()
+    url = f'https://graph.facebook.com/{user_id}?access_token={token}'
+    response = requests.get(url)
+    return response.json()
 
     
