94c86a576b479d5cb6d06c10279e73d7a748af90
jairo sandoval <jairo.sandoval.velasquez@gmail.com>
2021-10-11 20:10:02 -0700

    .

--- facebook_handler/profile_functions.py
+++ facebook_handler/profile_functions.py
@@ -1,10 +1,15 @@
 import requests
+from odoo.exceptions import ValidationError
+from odoo.exceptions import AccessDenied
 
 def get_info_profile(user, token):
     user_id = user["sender"]["id"]
 
-    url = f'https://graph.facebook.com/{user_id}?access_token={token}'
-    response = requests.get(url)
-    return response.json()
+    if not token:
+        raise ValidationError("user error")
+    else:
+        url = f'https://graph.facebook.com/{user_id}?access_token={token}'
+        response = requests.get(url)
+        return response.json()
 
     
