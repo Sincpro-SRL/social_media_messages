6f009ea5f0a4dd12acb64695147b666586cb4849
Jeffer <jeffer.barrera@sincpro.com.bo>
2021-11-16 16:38:09 -0500

    fix documentation method

--- models/social_media_messages.py
+++ models/social_media_messages.py
@@ -11,21 +11,21 @@ class HandlerMessenger(models.Model):
         """
         :param data:
             {
-                'object': 'page',
-                'entry': {},
-                'source': {
-                    'social_network': Red social de la cual es recibido o enviado el mensaje,
-                    'status_message': Estado del mensaje (RECEIVED, SENT, NO_SENT)
+                object: str -> proporcionado por API,
+                entry: dict -> proporcionado por API,
+                source: {
+                    social_network: str -> Red social de la cual es recibido o enviado el mensaje,
+                    status_message: str -> Estado del mensaje (RECEIVED, SENT, NO_SENT)
                 },
-                'attempts': Cantidad de intentos para enviar mensajes (re-sender),
-                'file_attached': Archivo adjunto al mensaje,
+                attempts: int -> Cantidad de intentos para enviar mensajes (re-sender),
+                file_attached: binary -> Archivo adjunto al mensaje,
             }
         :return:
-            "REGISTRO CREADO EXITOSAMENTE"
+            str -> "REGISTRO CREADO EXITOSAMENTE"
         """
 
         entry_data = data['entry'][0]
-        messaging = entry_data['messaging'][0]
+        messaging = entry_data['messaging'] [0]
         values = {
             'date_message': datetime.utcfromtimestamp(entry_data['time'] / 1000).strftime('%Y-%m-%d %H:%M:%S'),
             'page_id': entry_data['id'],
