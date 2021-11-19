0e54947d42bbdc2007e16151e14815b8a5e0cc01
Jeffer <jeffer.barrera@sincpro.com.bo>
2021-11-19 11:44:31 -0500

    refactor method storage_data

--- models/social_media_messages.py
+++ models/social_media_messages.py
@@ -3,41 +3,23 @@ from datetime import datetime
 from odoo import models, fields
 
 
-class ManagementMessage(models.Model):
-    _name = 'management.message'
-    _description = "Organización de información del mensaje de messenger Webhook"
+class ManagementData(models.Model):
+    _name = 'management.data'
+    _description = "Almacenamiento de información del mensaje"
 
-    def management_body(self, data):
-        """
-        :param data:
-            {
-                object: str -> proporcionado por API,
-                entry: dict -> proporcionado por API,
-                source: {
-                    social_network: str -> Red social de la cual es recibido o enviado el mensaje,
-                    status_message: str -> Estado del mensaje (RECEIVED, SENT, NO_SENT)
-                },
-                attempts: int -> Cantidad de intentos para enviar mensajes (re-sender),
-                file_attached: binary -> Archivo adjunto al mensaje,
-            }
-        :return:
-            str -> "REGISTRO CREADO EXITOSAMENTE"
-        """
-
-        entry_data = data['entry'][0]
-        messaging = entry_data['messaging'] [0]
+    def storage_data(self, **kwargs):
         values = {
-            'date_message': datetime.utcfromtimestamp(entry_data['time'] / 1000).strftime('%Y-%m-%d %H:%M:%S'),
-            'page_id': entry_data['id'],
-            'customer_id': messaging['sender']['id'],
-            'customer_message': messaging['message']['text'],
-            'social_network': data['source']['social_network'],
-            'status_message': data['source']['status_message'],
+            'page_id': kwargs['page_id'],
+            'customer_id': kwargs['customer_id'],
+            'date_message': datetime.utcfromtimestamp(kwargs['time'] / 1000).strftime('%Y-%m-%d %H:%M:%S'),
+            'customer_message': kwargs['message'],
+            'social_network': kwargs['social_network'],
+            'status_message': kwargs['status_message'],
             'attempts': None,
             'file_attached': None,
         }
         self.create(values)
-        return "REGISTRO CREADO EXITOSAMENTE"
+        return "REGISTRO GUARDADO EXITOSAMENTE"
 
     page_id = fields.Integer(string="ID de la página receptora", require=True)
     customer_id = fields.Integer(string="ID de usuario de Facebook", require=True)
@@ -47,5 +29,6 @@ class ManagementMessage(models.Model):
     status_message = fields.Char(string="Estado del mensaje", require=True)
     attempts = fields.Integer(string="Número de intentos de envío")
     file_attached = fields.Binary(string="Archivo adjunto")
+    contact = fields.Many2many("res.partner", "", string="Contacto Asociado al mensaje")
 
 
