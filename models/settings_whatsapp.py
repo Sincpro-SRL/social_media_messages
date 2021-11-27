2ab9febacf5ec14bc7fa8e15b68cbdb04d9657e8
Jeffer <jeffer.barrera@sincpro.com.bo>
2021-11-26 19:14:19 -0500

    fixes

--- models/settings_whatsapp.py
+++ models/settings_whatsapp.py
@@ -1,13 +1,14 @@
 from odoo import fields, models, api
 
+
 class WhatsappConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    whatsapp_crm= fields.Boolean("")
+    whatsapp_crm = fields.Boolean("")
     whatsapp_sales = fields.Boolean("")
     whatsapp_inventory = fields.Boolean("")
     whatsapp_token = fields.Char(string="token")
-    facebook_crm= fields.Boolean("")
+    facebook_crm = fields.Boolean("")
     facebook_sales = fields.Boolean("")
     facebook_inventory = fields.Boolean("")
     facebook_token = fields.Char(string="token")
@@ -18,7 +19,7 @@ class WhatsappConfigSettings(models.TransientModel):
         self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_sales', self.whatsapp_sales)
         self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_inventory', self.whatsapp_inventory)
         self.env['ir.config_parameter'].set_param('whatsapp.whatsapp_token', self.whatsapp_token)
-        
+
         self.env['ir.config_parameter'].set_param('facebook.facebook_crm', self.facebook_crm)
         self.env['ir.config_parameter'].set_param('facebook.facebook_sales', self.facebook_sales)
         self.env['ir.config_parameter'].set_param('facebook.facebook_inventory', self.facebook_inventory)
@@ -36,8 +37,7 @@ class WhatsappConfigSettings(models.TransientModel):
         facebook_sales = self.env['ir.config_parameter'].get_param("facebook.facebook_sales")
         facebook_inventory = self.env['ir.config_parameter'].get_param("facebook.facebook_inventory")
         facebook_token = self.env['ir.config_parameter'].sudo().get_param("facebook.facebook_token")
-        
-        
+
         res.update({
             'facebook_crm': facebook_crm,
             'facebook_sales': facebook_sales,
@@ -45,4 +45,3 @@ class WhatsappConfigSettings(models.TransientModel):
             'facebook_token': facebook_token,
         })
         return res
-
